48b292ea7198286c759bca733f4bc3d47a9131e5
iamriatandan <tandanria@gmail.com>
2025-02-21 15:45:26 +0530

    [Ria] Added . Uc2

--- src/main/java/com/springmessaging/app/HelloController.java
+++ src/main/java/com/springmessaging/app/HelloController.java
@@ -1,6 +1,7 @@
 package com.springmessaging.app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String uc1(){
         return "Hello from Bridgelabz";
+    }
+
+    //Respond to GET request at "/hello/query" with a name query parameter
+    @GetMapping("/hello/query")
+    public String uc2(@RequestParam String name){
+        return "Hello " + name + "Welcome to bridgelabz!";
     }
 }
